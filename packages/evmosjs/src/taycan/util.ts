092062eaaacc2728e8ee9f3b57463e8aab6ffab0
jh.koo <jh.koo@hupayx.com>
2022-09-16 06:15:32 +0000

    - number 수정

--- packages/evmosjs/src/taycan/util.ts
+++ packages/evmosjs/src/taycan/util.ts
@@ -1,21 +1,21 @@
 import { Coin }  from '@taycan/provider';
-
+import BigNumber from 'bignumber.js';
 const tokenUnits = {
     baseDenom : "asfl",
     displayDenom : "sfl",
     exponent : 18
 }
-
+const wei = '1000000000000000000';
 export const formatDenom = (amount: string ): Coin => {
-  var amountValue = Number(amount.substring(0, amount.indexOf(tokenUnits.baseDenom)));
+  var amountValue = amount.substring(0, amount.indexOf(tokenUnits.baseDenom));
 
   if(amount.indexOf(tokenUnits.baseDenom) == -1) {
-      var amountValue = Number(amount.substring(0, amount.indexOf(tokenUnits.displayDenom)));
-      amountValue *= 10 ** tokenUnits.exponent;
+      var amountValue = amount.substring(0, amount.indexOf(tokenUnits.displayDenom));
+      amountValue = new BigNumber(amountValue).multipliedBy(wei).toFixed();
   }
 
   return {
       denom: tokenUnits.baseDenom,
-      amount: amountValue.toString()
+      amount: amountValue
   }
 }
